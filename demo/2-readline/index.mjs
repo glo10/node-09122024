6355ed086c7cead05b09206c365a3a69ec62e0f1
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-12-09 12:49:10 +0100

    refactor: add details

--- demo/2-readline/index.mjs
+++ demo/2-readline/index.mjs
@@ -1,8 +1,17 @@
-// on importe uniquement la fonction createInterface depuis le module interne readline
+/**
+ * on importe uniquement la fonction createInterface depuis le module interne readline
+ * Ce module est constitué de plusieurs fonctions
+ * Ici on a juste besoin de createInterface donc on utilise la décomposation
+ * @see https://www.honeybadger.io/blog/javascript-destructuring/
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+*/
+
 import { createInterface } from 'node:readline/promises'
 
-// utilisation de la fonction avec les bons paramètres ici les interfaces pour la communication avec l'utilisateur
-// ici le terminal
+/**
+ * utilisation de la fonction avec les bons paramètres ici les interfaces pour la communication avec l'utilisateur
+ * ici le terminal
+ */ 
 const ui = createInterface({ input: process.stdin, output: process.stdout})
 /**
 * question() est asynchrone donc le programme n'attend le résultat avant d'exécuter
@@ -11,6 +20,14 @@ const ui = createInterface({ input: process.stdin, output: process.stdout})
 const response = ui.question('Quel est votre langage préferée ?')
 response.then((data) => {
   console.log('La réponse du user est ', data)
+  ui.close()
 })
-ui.close()
-// console.log('FIN')
+
+/**
+ * Attention si on avait écrit ui.close() en dehors de then
+ *  le programme ferme l'interface d'entrée immédiatement
+ *  juste après avoir posé la question
+ *  donc le programme ne donne pas la possibilité à l'utilisateur de faire la saisie
+ */
+// ui.close()
+ 
