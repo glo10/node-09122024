6eff4b80bf293d82a0280f69b0b60e203eec4111
formateur-glodiee-tshimini <contact@tshimini.fr>
2024-12-10 09:30:53 +0100

    refactor: add details

--- demo/5-web-server/rappel.js
+++ demo/5-web-server/rappel.js
@@ -5,10 +5,14 @@ const PORT = 8082
 const app = createServer(function(req, res){
   // req = requête du client
   // res = réponse du serveur
-  if(req.url === '/') {
-    // Ecriture au niveau du headers
+  if(req.url === '/') {// route / = page d'accueil
+    /**
+     * Ecriture au niveau du headers
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
+     * @see https://en.wikipedia.org/wiki/List_of_HTTP_header_fields 
+    */ 
     res.writeHead(200, { 'content-type': 'application/json'})
-    // Ecriture au niveau du body (corps de la réponse)
+    // Ecriture au niveau du body (corps de la réponse retourné au client)
     res.write('{ "message": "OK" }')
   } else {
     res.writeHead(404, { 'content-type': 'application/json'})
@@ -16,12 +20,16 @@ const app = createServer(function(req, res){
   }
   /**
    * Sans res.end() le client reste en attente de la réponse
-   * jusqu'à ce qu'il interrompt la communication
+   * jusqu'à ce qu'il interrompt la communication (timeout)
    */ 
   res.end()
 })
 
-// Mettre le serveur en écoute
+/**
+ * Mettre le serveur en écoute pour des connexions clientes
+ *  sur un port d'écoute
+ * Attention le port doit être libre = non utilisé par une autre application
+ */ 
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`)
 })
